892139e84010b94e4b7b7fef30c92b9eaaed109a
Mrsazza <shahriarhossain147@gmail.com>
2021-10-14 23:28:11 +0600

    Initial Commit

---
+++ KeyBoardExtension/KeyBoardExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyBoardExtensionApp.swift
+//  KeyBoardExtension
+//
+//  Created by Sazza on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct KeyBoardExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
